62d0ad464013ae873a434863a7e143367aead1a1
ThomasMarnet <thomas.marnet@gmail.com>
2015-06-02 13:30:09 +0200

    First commit

---
+++ _posts/2015-06-01-welcome-to-test.md
@@ -0,0 +1,31 @@
+---
+layout: post
+title:  "Welcome to Test!"
+date:   2015-06-01 14:49:34
+categories: jekyll update
+---
+## TEST
+You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
+
+To add new posts, simply add a file in the `_posts` directory that follows the convention `YYYY-MM-DD-name-of-post.ext` and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.
+
+… which is shown in the screenshot below:
+![My helpful screenshot](http://img.wikinut.com/img/gycf69_-6rv_5fol/jpeg/0/Best-Friends-Img-Src:Image:-FreeDigitalPhotos.net.jpeg)
+
+… you can [get the PDF](http://math.unice.fr/~pauly/ca1.pdf) directly
+
+Jekyll also offers powerful support for code snippets:
+
+{% highlight ruby linenos %}
+def print_hi(name)
+  puts "Hi, #{name}"
+end
+print_hi('Tom')
+#=> prints 'Hi, Tom' to TEST.
+{% endhighlight %}
+
+Check out the [Jekyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll’s GitHub repo][jekyll-gh]. If you have questions, you can ask them on [Jekyll’s dedicated Help repository][jekyll-help].
+
+[jekyll]:      http://jekyllrb.com
+[jekyll-gh]:   https://github.com/jekyll/jekyll
+[jekyll-help]: https://github.com/jekyll/jekyll-help
---
+++ js/clean-blog.js
@@ -0,0 +1,48 @@
+// make all images responsive
+$(function() {
+	$("img").addClass("img-responsive");
+});
+
+// responsive tables
+$(document).ready(function() {
+	$("table").wrap("<div class='table-responsive'></div>");
+	$("table").addClass("table");
+});
+
+// responsive embed videos
+$(document).ready(function () { 
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+});
+
+// Navigation Scripts to Show Header on Scroll-Up
+$(document).ready(function($) {
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height(),
+			introHeight = $('.intro-header').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+				if (currentTop >= introHeight) {
+					$('.navbar-custom').addClass('is-top');
+				} else {
+					$('.navbar-custom').removeClass('is-top');
+				}
+                this.previousTop = currentTop;
+            });
+    }
+});
+
+$(document).ready(function() {
+	$('.nav').click('a', function(e) {
+		$('.nav').find('li').removeClass('active');
+		$(this).parent.addClass('active');
+	})
+});
